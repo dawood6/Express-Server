89790e96d0304bdf74271a9ae32b2be00012213b
Dawood Sadiq <dawoodsadiq171@gmail.com>
2020-04-23 20:13:54 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ const app = express();
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send({name: 'dawood'})
+  res.send(Home)
+})
+app.get('/about', (req, res) => {
+  res.send(About)
+})
+app.get('/contact', (req, res) => {
+  res.send(Contact)
+})
+app.get('/info', (req, res) => {
+  res.send(Information)
 })
 
 app.listen(3001, () => console.log('server is running on port 3001'))
